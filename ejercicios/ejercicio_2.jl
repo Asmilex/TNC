dc35eac3b355b0334637095c94461ff0c0cd86eb
Asmilex <andresmm@outlook.com>
2022-02-21 19:23:43 +0100

    [:sparkles:} Ejercicio 1 listo y plantilla del 2

---
+++ ejercicios/ejercicio_2.jl
@@ -0,0 +1,36 @@
+### A Pluto.jl notebook ###
+# v0.18.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 328a2950-9343-11ec-28ec-7139a5c072a1
+md"""
+
+# Ejercicio 2 
+
+Dado tu número n de 8 cifras de la lista publicada.
+1. Usa el algoritmo manual para calcular el símbolo de Jacobi ($\frac{p}{n}$), para p cada uno de los 5 primeros primos.
+2. Si para alguna de esas bases tu número sale posible primo de Fermat, comprueba si además es posible primo de Euler.
+3. ¿Es tu número n pseudoprimo de Fermat o de Euler para alguna de las bases?
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─328a2950-9343-11ec-28ec-7139a5c072a1
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
