8a8d57a2c968bb04fe53155933961fc72d0b5aa3
Asmilex <andresmm@outlook.com>
2022-03-01 17:50:10 +0100

    [:sparkles:] Plantilla para el ejercicio 4

---
+++ ejercicios/ejercicio_4.jl
@@ -0,0 +1,38 @@
+### A Pluto.jl notebook ###
+# v0.18.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 3d9b74e0-9959-11ec-3543-cda7e1f95c77
+md"""
+# Ejercicio 4 
+
+Dado tu número $n$ de 8 cifras de la lista del ejercicio 2:
+
+1. Facroriza $n-1$ aplicando el método $\rho$ de Polard. ¿Cuántas iteraciones necesitas?
+2. Si es necesario aplica recursivamente Lucas-Lehmer para certificar factores primos de $n-1$ mayores de 4 cifras.
+3. Aplica Lucas-Lehmer para encontrar un certificado de primalidad de $n$.
+
+> NOTA: Debes encontrar el natural más pequeño cuya clase sea primitiva.
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─3d9b74e0-9959-11ec-3543-cda7e1f95c77
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
