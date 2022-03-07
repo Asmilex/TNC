cb02a80876ae26baebb2accc442b7861b913c44b
Asmilex <andresmm@outlook.com>
2022-03-07 11:38:01 +0100

    [:sparkles:] Enunciados del 5 y 6

---
+++ ejercicios/ejercicio_5, 6.jl
@@ -0,0 +1,49 @@
+### A Pluto.jl notebook ###
+# v0.18.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 33d1dcf0-9df6-11ec-052c-db0b1f6e9906
+md"""
+# Ejercicio 5
+
+Dado tu número $n$ de la lista publicada para este ejercicio:
+
+1. Factoriza $n$ aplicando el método $\rho$ de Polard. ¿Cúantas iteraciones necesitas?
+Sea $p1$ el mayor de sus factores primos y $p2$ el siguiente primo.
+2. Calcula las partes enteras de $p1$ y $p2$ con el algoritmo entero.
+3. Calcula las FCS de $p1$ y $p2$ aplicando el algoritmo que usa aritmética entera
+"""
+
+# ╔═╡ 54f11d9d-da5a-4c17-9623-50806eb25117
+md"""
+# Ejercicio 6
+
+Sea $p$ el factor primo que tiene mayor periodo.
+1. Calcula los convergentes de $p$.
+2. Calcula las soluciones de las ecuaciones de Pell, $x^2 - py^2 = \pm 1$
+3. Calcula las unidades del anillo de enteros cuadráticos $\mathbb{Z}[p]$.
+4. ¿Es $\mathbb{Z}[p]$ el anillo de enteros del cuerpo $\mathbb{Q}[p]$? 
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─33d1dcf0-9df6-11ec-052c-db0b1f6e9906
+# ╟─54f11d9d-da5a-4c17-9623-50806eb25117
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
