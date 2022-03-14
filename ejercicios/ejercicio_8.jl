a0ef19b708c4858b199d0d811f5f3a5b65a784c1
Asmilex <andresmm@outlook.com>
2022-03-14 13:24:03 +0100

    Añadidos los enunciados del 7 y 8

---
+++ ejercicios/ejercicio_8.jl
@@ -0,0 +1,40 @@
+### A Pluto.jl notebook ###
+# v0.18.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 62135ae0-a38b-11ec-10aa-77dabd6a8446
+md"""
+# Ejercicio 8 
+
+Toma tu número $n$ de la lista publicada para este ejercicio. 
+
+1. Pasa algunos tests de primalidad para ver si n es compuesto.
+2. En caso que tu $n$ sea probable primo, factoriza $n+1$ encontrando certificados de primalidad para los factores de $10000$.
+3. Con $P = 1$, encuentra el menos $Q \ge 2$ tal que definan una sucesión de Lucas que certifique la primalidad de $n$ 
+"""
+
+# ╔═╡ a1404118-b17a-43ae-9c26-587b6851f6b4
+n = 2783584280956932824984484769172860189421
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─62135ae0-a38b-11ec-10aa-77dabd6a8446
+# ╟─a1404118-b17a-43ae-9c26-587b6851f6b4
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
